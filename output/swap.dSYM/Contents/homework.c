f655c80adb8a69f81aa242341759e5e0d65795ee
DolmaTsherpa <dolmachhhiring@gmail.;com>
2025-05-15 19:59:41 +0545

    committed by Dolma Tsering Sherpa on 15 may, 7:59pm

---
+++ output/swap.dSYM/Contents/homework.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int roll =12, age =19;
+    char gender = 'f';
+    char name[20]= "Dolma Tsering Sherpa";
+    printf("Roll number = %d", roll);
+    printf("\nName = %s", name);
+    printf("\nAge = %d", age);
+    printf("\nGender = %c", gender);
+    return 0;
+
+
+}
